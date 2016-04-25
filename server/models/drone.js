8ab7b0745bae7cfa3080ab30bfba8455fa9c8f28
Antoine Brezillon <antoine.brezillon@gmail.com>
2016-04-25 15:46:03 +0200

    clean drone.js code

--- server/models/drone.js
+++ server/models/drone.js
@@ -29,26 +29,8 @@ module.exports = function(Drone) {
   );
 
   //TODO add remote method missionCall
-  /*
-  Drone.afterRemote('mission', function(ctx, modelInstance, next) {
-    // Model instance is the returned array
-    if(Array.isArray(modelInstance)){
-      console.log(" modelInstance is array");
-      ctx.result.forEach(function(intervention){
-        var temp = {};
-        temp[intervention]=ctx.result[intervention];
-        // Here creationDate is a field of my intervention adapt it mtfk !! :)
-        console.log("   creationDate : " +intervention.creationDate);
-      })
-    }
-    next();
-  });
-  */
 
   //TODO add sanitizing before methods
 
   //TODO add auth before methods
-
-  //TODO add mission call after method missionCall
-
 };
