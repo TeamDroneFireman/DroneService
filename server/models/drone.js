617ed1b627b33ee0231150dbb618cb8a6835c127
Antoine Brezillon <antoine.brezillon@gmail.com>
2016-04-27 11:18:37 +0200

    drone mission planner added

--- server/models/drone.js
+++ server/models/drone.js
@@ -1,12 +1,13 @@
 module.exports = function(Drone) {
 
+  const DRONE_SCRIPTS_URL = "/home/kozhaa/Master2/project/DroneKit/mission_boucle.py";
+
   // Removes (DELETE) /products/:id
   Drone.disableRemoteMethod('deleteById', true);
   // Removes (POST) /products/update
   Drone.disableRemoteMethod('updateAll', true);
   // removes (GET|POST) /products/change-stream
   Drone.disableRemoteMethod('createChangeStream', true);
-
 
   /**
    * return all the drones used in the intervention passed as parameter
@@ -22,29 +23,51 @@ module.exports = function(Drone) {
   Drone.remoteMethod(
     'getByIntervention',
     {
-      http: {path: '/idIntervention/:id', verb: 'get'},
+      http: {path: '/intervention/:id', verb: 'get'},
       accepts: {arg: 'id', type: 'number', required: true},
       returns: {type: 'array', root: true}
     }
   );
 
-  //TODO add remote method missionCall
-  Drone.setMission = function(id, callback) {
+  /**
+   * set a new mission to the specified drone
+   * @param id
+   * @param mission
+   * @param callback
+     */
+  Drone.setMission = function(id, mission, callback) {
 
+    Drone.updateAll({"id" : id}, {"mission" : mission}, function(err, drone){
+      // get drone state from update response
+      // call mission manager in order to launch the mission (initialize if needed, based on state)
+      const spawn = require('child_process').spawn;
+      var arguments = [DRONE_SCRIPTS_URL, "--mission"];
+      for(var point in mission.poi){
+        arguments.push(mission.poi[point].x);
+        arguments.push(mission.poi[point].y);
+        arguments.push(mission.poi[point].z);
+      }
+      const mission_script = spawn('python', arguments);
+      callback(null, drone);
+    });
   };
 
   Drone.remoteMethod(
     'setMission',
     {
       http: {path: '/:id/mission/', verb: 'put'},
-      accepts: {arg: 'mission', type: 'object', http: {source: 'body'}},
+      accepts: [
+        {arg: 'id', type: 'number', required: true},
+        {arg: 'mission', type: 'object', http: {source: 'body'}}
+      ],
       returns: {type: 'array', root: true}
     }
   );
 
-  //TODO add sanitizing before methods
-
-  //TODO add auth before all methods
+  /***
+   * auth required before all methods
+   */
+  /*
   Drone.beforeRemote('*', function(ctx, unused, next) {
     Drone.app.datasources.auth
       .checkAuth(ctx.req.headers.userid, ctx.req.headers.token,
@@ -58,4 +81,6 @@ module.exports = function(Drone) {
           }
         });
   });
+  */
+  //TODO add sanitizing body obj before each methods
 };
