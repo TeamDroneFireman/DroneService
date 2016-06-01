367f1a34600deba36d5cf913cc8948f564c65b81
thomas_huchede <huchede.thomas@gmail.com>
2016-06-01 17:41:29 +0200

    fix auto naming if already numbered

--- server/models/drone.js
+++ server/models/drone.js
@@ -33,19 +33,43 @@ module.exports = function(Drone) {
     */
    Drone.beforeRemote('create', function(ctx, unused, next){
      var model = ctx.args.data;
-     var rePattern = new RegExp(/(.*?)\s*?(\d+)?$/);
-     var str = model.name.replace(rePattern, '$1');
-     Drone.count({intervention: model.intervention, name: {like: str} },
-       function(err, res){
-         model.name = model.name + ' ' + (res+1);
+     var dPattern = new RegExp(/\d+$/);
+    var hasNum = dPattern.test(model.name);
+    if (!hasNum){
+      var rePattern = new RegExp(/(.*?)\s*?(\d+)?$/);
+      var str = model.name.replace(rePattern, '$1');
+      Drone.count({intervention: model.intervention, name: {like: str} },
+        function(err, res){
+          model.name = model.name + ' ' + (res+1);
 
-         //Simulator specific code
-         Drone.count(function(err, res){
-           model.instance = res+1;
-           next();
-         });
-     });
+          //Simulator specific code
+          Drone.count(function(err, res){
+            model.instance = res+1;
+            next();
+          });
+      });
+    } else {
+      //Simulator specific code
+      Drone.count(function(err, res){
+        model.instance = res+1;
+        next();
+      });
+    }
    });
+
+
+    var rePattern = new RegExp(/(.*?)\s*?(\d+)?$/);
+    var str = model.name.replace(rePattern, '$1');
+    Drone.count({intervention: model.intervention, name: {like: str} },
+      function(err, res){
+        model.name = model.name + ' ' + (res+1);
+
+        //Simulator specific code
+        Drone.count(function(err, res){
+          model.instance = res+1;
+          next();
+        });
+    });
 
   /**
    * return all the drones used in the intervention passed as parameter
